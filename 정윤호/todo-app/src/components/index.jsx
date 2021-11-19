055d68946e561fc0c79615afc92d6448b80e7ab0
정윤호 <41064875+younoah@users.noreply.github.com>
2021-11-19 17:35:15 +0900

    Delete 정윤호 directory

--- 정윤호/todo-app/src/components/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as TodoTemplate } from './TodoTemplate';
-export { default as TodoCreate } from './TodoCreate';
-export { default as TodoHead } from './TodoHead';
-export { default as TodoItem } from './TodoItem';
-export { default as TodoList } from './TodoList';
